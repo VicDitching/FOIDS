5cd6e0dda2b893e73d89ae5e41c805d19c9fbd7c
Vic_Ditching <67437207+VicDitching@users.noreply.github.com>
2026-03-28 11:34:49 -0700

    Update script.py
    Added the dimensions of the software, added coded to load_configuration, and confirmation.

--- script.py
+++ script.py
@@ -6,13 +6,14 @@ from pathlib import Path
 
 class FOIDS:
     def __initialization__(self, root):
-        #Initialize the main application window
+        #SDD_LDD_PY_GUI_001: Initialize the main application window
         #Trace Tags: FR-04, IR-02
         self.root = root
         self.root.title("FOIDS Cleanup Tool")
-        self.root.geometry("")
+        self.root.geometry("450x500")
 
         #Checks if tool is being used in Windows 11
+        #Trace Tags: ESR-01, NFR-01
         if platform.system() != "Windows":
             messagebox.showerror("OS Error", "This tool is specifically for Windows 11")
             self.root.destroy()
@@ -28,7 +29,15 @@ class FOIDS:
         self.create_components()
 
     def load_configuration(self): 
-        
+        #DC-02: uses the python 'json' module to parse the rules.json file 
+        #Trace Tags: [SDD_HLD_CONFIG_001]
+        try: 
+            with open('rules.json', 'r') as f: 
+                return json.load(f)
+            except Exception as e: 
+                #error handling if JSON is missing or malformed 
+                print(f"Error loading configuration: {e}")
+                return {}
     
     def create_components(self):
 
@@ -37,7 +46,10 @@ class FOIDS:
 
     
     def confirmation(self):
-        
+        #safety check to ensure user intent before permanently deleting the selected files
+        #Trace Tags: FR-05, SR-03
+        if messagebox.askyesno("Confirm Deletion", "Are you sure? This will permanently delete the selected files."):
+            self.run_deletion()
     
     def deletion(self):
 
